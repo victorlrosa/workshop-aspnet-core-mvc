375e8300c7cec8fd5213660979cbac23e626b160
Victor Luiz <victorlrosa@hotmail.com>
2020-09-01 23:10:05 -0300

    Criando as outras entidades e a segunda migration.

--- SalesWebMvc/Models/Entities/Department.cs
+++ SalesWebMvc/Models/Entities/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models.Entities
 {
@@ -9,6 +8,25 @@ namespace SalesWebMvc.Models.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department()
+        {
+        }
 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Entities/Enums/SaleStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMvc.Models.Entities.Enums
+{
+    public enum SaleStatus
+    {
+        Pending, Billed, Canceled
+    }
+}
