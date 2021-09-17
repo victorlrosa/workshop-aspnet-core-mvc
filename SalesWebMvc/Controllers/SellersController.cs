13c72cac578b11b09462e5f14dfc16b2053e0b77
Victor Luiz <victorlrosa@hotmail.com>
2021-09-17 12:00:59 -0300

    Integridade referencial.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -63,8 +63,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Can't delete seller because he/she has sales"});
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
